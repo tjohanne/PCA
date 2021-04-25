e44381e0b267bcf544033ffe560babb099d44ae2
Tomas <tjohanne@andrew.cmu.edu>
2021-04-24 23:13:52 -0400

    eigensolver into main

--- eigensolver.cu
+++ eigensolver.cu
@@ -1,143 +1,133 @@
 /*
  * How to compile (assume cuda is installed at /usr/local/cuda/)
- *   nvcc -c -I/usr/local/cuda/include syevd_example.cpp 
+ *   nvcc -c -I/usr/local/cuda/include syevd_example.cpp
  *   g++ -o a.out syevd_example.o -L/usr/local/cuda/lib64 -lcudart -lcusolver
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <cuda_runtime.h>
 #include <cusolverDn.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void printMatrix(int m, int n, const double*A, int lda, const char* name)
-{
-    for(int row = 0 ; row < m ; row++){
-        for(int col = 0 ; col < n ; col++){
-            double Areg = A[row + col*lda];
-            printf("%s(%d,%d) = %f\n", name, row+1, col+1, Areg);
-        }
+void printMatrix(int m, int n, const double *A, int lda, const char *name) {
+  for (int row = 0; row < m; row++) {
+    for (int col = 0; col < n; col++) {
+      double Areg = A[row + col * lda];
+      printf("%s(%d,%d) = %f\n", name, row + 1, col + 1, Areg);
     }
+  }
 }
 
-int main(int argc, char*argv[])
-{
-    cusolverDnHandle_t cusolverH = NULL;
-    cusolverStatus_t cusolver_status = CUSOLVER_STATUS_SUCCESS;
-    cudaError_t cudaStat1 = cudaSuccess;
-    cudaError_t cudaStat2 = cudaSuccess;
-    cudaError_t cudaStat3 = cudaSuccess;
-    const int m = 3;
-    const int lda = m;
-/*       | 3.5 0.5 0 |
- *   A = | 0.5 3.5 0 |
- *       | 0   0   2 |
- *
- */
-    double A[lda*m] = { 3.5, 0.5, 0, 0.5, 3.5, 0, 0, 0, 2.0};
-    double lambda[m] = { 2.0, 3.0, 4.0};
+int eigensolver_example() {
+  cusolverDnHandle_t cusolverH = NULL;
+  cusolverStatus_t cusolver_status = CUSOLVER_STATUS_SUCCESS;
+  cudaError_t cudaStat1 = cudaSuccess;
+  cudaError_t cudaStat2 = cudaSuccess;
+  cudaError_t cudaStat3 = cudaSuccess;
+  const int m = 3;
+  const int lda = m;
+  /*       | 3.5 0.5 0 |
+   *   A = | 0.5 3.5 0 |
+   *       | 0   0   2 |
+   *
+   */
+  double A[lda * m] = {3.5, 0.5, 0, 0.5, 3.5, 0, 0, 0, 2.0};
+  double lambda[m] = {2.0, 3.0, 4.0};
 
-    double V[lda*m]; // eigenvectors
-    double W[m]; // eigenvalues
+  double V[lda * m]; // eigenvectors
+  double W[m];       // eigenvalues
 
-    double *d_A = NULL;
-    double *d_W = NULL;
-    int *devInfo = NULL;
-    double *d_work = NULL;
-    int  lwork = 0;
+  double *d_A = NULL;
+  double *d_W = NULL;
+  int *devInfo = NULL;
+  double *d_work = NULL;
+  int lwork = 0;
 
-    int info_gpu = 0;
+  int info_gpu = 0;
 
-    printf("A = (matlab base-1)\n");
-    printMatrix(m, m, A, lda, "A");
-    printf("=====\n");
- 
-// step 1: create cusolver/cublas handle
-    cusolver_status = cusolverDnCreate(&cusolverH);
-    assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+  printf("A = (matlab base-1)\n");
+  printMatrix(m, m, A, lda, "A");
+  printf("=====\n");
 
-// step 2: copy A and B to device
-    cudaStat1 = cudaMalloc ((void**)&d_A, sizeof(double) * lda * m);
-    cudaStat2 = cudaMalloc ((void**)&d_W, sizeof(double) * m);
-    cudaStat3 = cudaMalloc ((void**)&devInfo, sizeof(int));
-    assert(cudaSuccess == cudaStat1);
-    assert(cudaSuccess == cudaStat2);
-    assert(cudaSuccess == cudaStat3);
+  // step 1: create cusolver/cublas handle
+  cusolver_status = cusolverDnCreate(&cusolverH);
+  assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
 
-    cudaStat1 = cudaMemcpy(d_A, A, sizeof(double) * lda * m, cudaMemcpyHostToDevice);
-    assert(cudaSuccess == cudaStat1);
+  // step 2: copy A and B to device
+  cudaStat1 = cudaMalloc((void **)&d_A, sizeof(double) * lda * m);
+  cudaStat2 = cudaMalloc((void **)&d_W, sizeof(double) * m);
+  cudaStat3 = cudaMalloc((void **)&devInfo, sizeof(int));
+  assert(cudaSuccess == cudaStat1);
+  assert(cudaSuccess == cudaStat2);
+  assert(cudaSuccess == cudaStat3);
 
-// step 3: query working space of syevd
-    cusolverEigMode_t jobz = CUSOLVER_EIG_MODE_VECTOR; // compute eigenvalues and eigenvectors.
-    cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
-    cusolver_status = cusolverDnDsyevd_bufferSize(
-        cusolverH,
-        jobz,
-        uplo,
-        m,
-        d_A,
-        lda,
-        d_W,
-        &lwork);
-    assert (cusolver_status == CUSOLVER_STATUS_SUCCESS);
+  cudaStat1 =
+      cudaMemcpy(d_A, A, sizeof(double) * lda * m, cudaMemcpyHostToDevice);
+  assert(cudaSuccess == cudaStat1);
 
-    cudaStat1 = cudaMalloc((void**)&d_work, sizeof(double)*lwork);
-    assert(cudaSuccess == cudaStat1);
+  // step 3: query working space of syevd
+  cusolverEigMode_t jobz =
+      CUSOLVER_EIG_MODE_VECTOR; // compute eigenvalues and eigenvectors.
+  cublasFillMode_t uplo = CUBLAS_FILL_MODE_LOWER;
+  cusolver_status = cusolverDnDsyevd_bufferSize(cusolverH, jobz, uplo, m, d_A,
+                                                lda, d_W, &lwork);
+  assert(cusolver_status == CUSOLVER_STATUS_SUCCESS);
 
-// step 4: compute spectrum
-    cusolver_status = cusolverDnDsyevd(
-        cusolverH,
-        jobz,
-        uplo,
-        m,
-        d_A,
-        lda,
-        d_W,
-        d_work,
-        lwork,
-        devInfo);
-    cudaStat1 = cudaDeviceSynchronize();
-    assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
-    assert(cudaSuccess == cudaStat1);
+  cudaStat1 = cudaMalloc((void **)&d_work, sizeof(double) * lwork);
+  assert(cudaSuccess == cudaStat1);
 
-    cudaStat1 = cudaMemcpy(W, d_W, sizeof(double)*m, cudaMemcpyDeviceToHost);
-    cudaStat2 = cudaMemcpy(V, d_A, sizeof(double)*lda*m, cudaMemcpyDeviceToHost);
-    cudaStat3 = cudaMemcpy(&info_gpu, devInfo, sizeof(int), cudaMemcpyDeviceToHost);
-    assert(cudaSuccess == cudaStat1);
-    assert(cudaSuccess == cudaStat2);
-    assert(cudaSuccess == cudaStat3);
+  // step 4: compute spectrum
+  cusolver_status = cusolverDnDsyevd(cusolverH, jobz, uplo, m, d_A, lda, d_W,
+                                     d_work, lwork, devInfo);
+  cudaStat1 = cudaDeviceSynchronize();
+  assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+  assert(cudaSuccess == cudaStat1);
 
-    printf("after syevd: info_gpu = %d\n", info_gpu);
-    assert(0 == info_gpu);
+  cudaStat1 = cudaMemcpy(W, d_W, sizeof(double) * m, cudaMemcpyDeviceToHost);
+  cudaStat2 =
+      cudaMemcpy(V, d_A, sizeof(double) * lda * m, cudaMemcpyDeviceToHost);
+  cudaStat3 =
+      cudaMemcpy(&info_gpu, devInfo, sizeof(int), cudaMemcpyDeviceToHost);
+  assert(cudaSuccess == cudaStat1);
+  assert(cudaSuccess == cudaStat2);
+  assert(cudaSuccess == cudaStat3);
 
-    printf("eigenvalue = (matlab base-1), ascending order\n");
-    for(int i = 0 ; i < m ; i++){
-        printf("W[%d] = %E\n", i+1, W[i]);
-    }
+  printf("after syevd: info_gpu = %d\n", info_gpu);
+  assert(0 == info_gpu);
 
-    printf("V = (matlab base-1)\n");
-    printMatrix(m, m, V, lda, "V");
-    printf("=====\n");
+  printf("eigenvalue = (matlab base-1), ascending order\n");
+  for (int i = 0; i < m; i++) {
+    printf("W[%d] = %E\n", i + 1, W[i]);
+  }
 
-// step 4: check eigenvalues
-    double lambda_sup = 0;
-    for(int i = 0 ; i < m ; i++){
-        double error = fabs( lambda[i] - W[i]);
-        lambda_sup = (lambda_sup > error)? lambda_sup : error;
-    }
-    printf("|lambda - W| = %E\n", lambda_sup);
+  printf("V = (matlab base-1)\n");
+  printMatrix(m, m, V, lda, "V");
+  printf("=====\n");
 
-// free resources
-    if (d_A    ) cudaFree(d_A);
-    if (d_W    ) cudaFree(d_W);
-    if (devInfo) cudaFree(devInfo);
-    if (d_work ) cudaFree(d_work);
+  // step 4: check eigenvalues
+  double lambda_sup = 0;
+  for (int i = 0; i < m; i++) {
+    double error = fabs(lambda[i] - W[i]);
+    lambda_sup = (lambda_sup > error) ? lambda_sup : error;
+  }
+  printf("|lambda - W| = %E\n", lambda_sup);
 
-    if (cusolverH) cusolverDnDestroy(cusolverH);
+  // free resources
+  if (d_A)
+    cudaFree(d_A);
+  if (d_W)
+    cudaFree(d_W);
+  if (devInfo)
+    cudaFree(devInfo);
+  if (d_work)
+    cudaFree(d_work);
 
-    cudaDeviceReset();
+  if (cusolverH)
+    cusolverDnDestroy(cusolverH);
 
-    return 0;
+  cudaDeviceReset();
+
+  return 0;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 
 void cuda_example();
 int cublas_example();
-
+int eigensolver_example();
 int main() {
   cublas_example();
+  eigensolver_example();
   return 1;
 }
