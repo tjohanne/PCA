fcc7a292f111f540d947e21c7128da65fa124c18
Tomas <tjohanne@andrew.cmu.edu>
2021-05-06 19:21:27 -0400

    merge conflict in script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 export PATH="/usr/local/cuda-11.2/bin:$PATH" &&
 export LD_LIBRARY_PATH="/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH" &&
 
-cd src && 
+cd ../src && 
 make clean && make -j 32 &&
 cd ../objs &&
 
@@ -16,12 +16,8 @@ MAXSWEEPS=150
 ECON=1
 VERBOSITY=0
 echo "MNIST PCA" &&
-<<<<<<< HEAD:run.sh
-./cudaPca mnist_784.csv 783
-=======
 ./cudaPca $DATA $NCOMP $TOL $MAXSWEEPS $ECON $VERBOSITY
 
->>>>>>> 18944509130c568535235fa95c68061d9fe614ed:scripts/run.sh
 
 # echo "EIGENFACES PCA"
 # ./cudaPca face_data.csv 400
